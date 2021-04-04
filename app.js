b8de1223c15cdc65671496361d38e5dfa1b35430
EL Khan <elkhanmursali@gmail.com>
2021-04-04 15:02:27 +0400

    Adding logic for updating UI according selected app

--- app.js
+++ app.js
@@ -3,9 +3,41 @@ const circle = document.querySelector('#circle');
 let rotateValue = circle.style.transform;
 let rotatePosition;
 
-circle.addEventListener('click', function() {
+let details = [
+    {id: 1, text: "Aga Bala", img: "images/agabala_detail.png", appstore: "https://apps.apple.com/us/app/aga-bala/id1548189656", googleplay: "https://play.google.com/store/apps/details?id=com.EKOGames.AgaBala"},
+    {id: 2, text: "Easy Useful Wallet", img: "images/mywallet_detail.png", appstore: "https://apps.apple.com/az/app/easy-useful-wallet/id1548000262", googleplay: "https://play.google.com/store/apps/developer?id=Elkhan+Mursali"},
+    {id: 3, text: "Space War", img: "images/spacewar_detail.png", appstore: "https://apps.apple.com/us/app/space-war-6/id1561462670", googleplay: "https://play.google.com/store/apps/details?id=com.EKOGames.SpaceWar"}
+]
+let currentId = 1;
+let detailText = document.getElementById('appHeader');
+let detailImage = document.getElementById('appImage');
+let appstore = document.getElementById('appstore');
+let googleplay = document.getElementById('googleplay');
+
+circle.addEventListener('click', function(){
     //circle.classList.toggle('circle-active');
+    rotateDisc();
+    updateDetails();
+});
+
+function rotateDisc(){
     rotatePosition = rotateValue + "rotate(90deg)";
     circle.style.transform = rotatePosition;
     rotateValue = rotatePosition;
-});
+}
+
+function updateDetails(){
+    detailText.innerHTML = getNextDetailText();
+    detailImage.src = details[currentId].img;
+    appstore.href = details[currentId].appstore;
+    googleplay.href = details[currentId].googleplay;
+    currentId++;
+    // reset
+    if(currentId == details.length){
+        currentId = 0;
+    }
+}
+
+function getNextDetailText(){
+    return details[currentId].text;
+}
